a3d64dff29f3a9543805875e5e75d2970024daff
EC2 Default User <ec2-user@ip-172-31-51-9.ec2.internal>
2026-04-19 23:18:13 +0000

    Ajustes de logica na conexão/Fix erros

--- server.py
+++ server.py
@@ -6,9 +6,13 @@ import threading
 
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind(('0.0.0.0', PORT))  #-
-s.listen(1)           #-
+s.listen(50)           #-
 
-def handle_req(data, conn):
+def handle_req(conn):
+    data = conn.recv(1024)   # receive data from client
+    if not data:
+        conn.close()	
+        return 
     dados = bytes.decode(data)
     array = json.loads(data)
     operation = array[0]
@@ -19,21 +23,14 @@ def handle_req(data, conn):
     if(operation == 0): res = op1 + op2
     elif(operation == 1): res = op1 - op2
     elif(operation == 2): res = op1 * op2
-    print("Data recieved from client: " + bytes.decode(data))
+    print("Data recieved from client: " + dados)
     conn.send(str.encode(str(res)))
-
+    conn.close()
+	
 threads = []
 
-(conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
-    data = conn.recv(1024)   # receive data from client
-    if data:
-      t = threading.Thread(target=handle_req, args=(data, conn))
-      threads.append(t)
-      t.start()
-    else: 
-      conn.close()
-      break
-
-for thr in threads:
-    thr.join()
+    (conn, addr) = s.accept()  # returns new socket and addr. client 
+    t = threading.Thread(target=handle_req, args=(conn,))
+    t.start()
+    threads.append(t)
